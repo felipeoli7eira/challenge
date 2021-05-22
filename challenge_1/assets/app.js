7d2b0ae74d8c97047badd635f86f230b5d952fbb
felipeoli7eira <me.felipeoliveira@gmail.com>
2021-05-21 23:02:50 -0300

    criando um controller e um model

--- challenge_1/assets/app.js
+++ challenge_1/assets/app.js
@@ -1,1 +1,11 @@
-console.log('running...')
+export default class App
+{
+    contructor()
+    {
+        this.app = {
+            api_url: "https://api.github.com"
+        }
+
+        console.log('running...')
+    }
+}
