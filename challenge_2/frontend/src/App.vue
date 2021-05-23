3a4fa98789c594330761f78610e3686133bf4740
felipeoli7eira <me.felipeoliveira@gmail.com>
2021-05-22 21:50:07 -0300

    iniciando a tela de cadastro de produto

--- challenge_2/frontend/src/App.vue
+++ challenge_2/frontend/src/App.vue
@@ -1,5 +1,21 @@
 <template>
-  <h1>welcome</h1>
+  <div id="app">
+    <div class="container mt-5 border border-dark">
+      <div class="row">
+        <div class="col col-6 border border-danger">
+          <form class="p-5 product-data">
+            <input type="text" name="product_name" class="form-control mb-1" placeholder="Nome">
+            <input type="text" name="product_desc" class="form-control mb-1" placeholder="Descrição">
+            <input type="text" name="product_price" class="form-control mb-1" placeholder="Preço">
+            <button type="button" class="btn btn-primary btn-block">salvar</button>
+          </form>
+        </div>
+        <div class="col col-6 border border-danger">
+          2
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,6 +31,16 @@
 *
 {
   font-family: 'Noto Sans JP', sans-serif !important;
+}
+
+body
+{
+  background-color: #eaeaea !important;
+}
+
+form.product-data button
+{
+  text-transform: uppercase;
 }
 
 </style>
