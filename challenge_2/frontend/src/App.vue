f656504b416be200674f75efbee5925ab887f9fb
felipeoli7eira <me.felipeoliveira@gmail.com>
2021-05-25 09:37:14 -0300

    melhoria de layout

--- challenge_2/frontend/src/App.vue
+++ challenge_2/frontend/src/App.vue
@@ -1,33 +1,68 @@
 <template>
   <div id="app">
-    <div class="text-center mt-5">
-      <h1>CRUD de produtos</h1>
-    </div>
 
     <div class="container mt-5">
+
+      <div class="text-center mx-auto p-3 rounded shadow my-5 title-screen">
+        <h2>CRUD de produtos</h2>
+      </div>
+
       <div class="row">
 
-        <div class="col col-4">
-          <form class="p-1 product-data pt-5">
-            <input v-model="product.name" type="text" name="product_name" class="form-control mb-1" placeholder="Nome">
-            <input v-model="product.desc" type="text" name="product_desc" class="form-control mb-1" placeholder="Descrição">
-            <input v-model="product.price" type="text" name="product_price" class="form-control mb-3" placeholder="Preço">
-            <button @click="submit()" type="button" class="btn btn-primary btn-block">{{create ? 'salvar' : 'atualizar'}}</button>
+        <div class="col col-4 p-4 rounded bg-white shadow">
+          <form class="product-data">
+            <div class="form-group mb-2">
+              <label>Produto</label>
+              <input
+                v-model="product.name"
+                type="text"
+                class="form-control"
+                placeholder="..."
+              />
+            </div>
+
+            <div class="form-group mb-2">
+              <label>Descrição</label>
+              <input
+                v-model="product.desc"
+                type="text"
+                class="form-control"
+                placeholder="..."
+              />
+            </div>
+
+            <div class="form-group mb-2">
+              <label>Preço</label>
+              <input
+                v-model="product.price"
+                type="text"
+                class="form-control"
+                placeholder="0,00"
+              />
+            </div>
+
+            <button
+              @click="submit()"
+              type="button"
+              class="btn btn-primary">
+              {{create ? 'salvar' : 'atualizar'}}
+            </button>
           </form>
         </div>
 
         <div class="col col-8">
           <div class="text-center">
-            <h4>lista de produtos</h4>
+            <h4 class="rounded shadow p-3 bg-white">Lista de produtos</h4>
           </div>
 
           <button @click="getProducts()">buscar</button>
           <table class="table table-striped">
               <thead>
                 <tr>
-                  <th scope="col">produto</th>
-                  <th scope="col">descrição</th>
-                  <th scope="col">preço</th>
+                  <th class="bg-white border-0 rounded" scope="col">produto</th>
+                  <th class="bg-white border-0 rounded" scope="col">descrição</th>
+                  <th class="bg-white border-0 rounded" scope="col">preço</th>
+                  <th class="bg-white border-0 rounded" scope="col">ação</th>
                 </tr>
               </thead>
               <tbody>
@@ -36,8 +71,8 @@
                   <td>{{product.desc}}</td>
                   <td>R$ {{product.price}}</td>
                   <td>
-                    <button @click="destroy(product._id)" class="btn btn-danger btn-sm">deletar</button>
-                    <button @click="edit(product._id)" class="btn btn-secondary btn-sm">editar</button>
+                    <button @click="destroy(product._id)" class="btn btn-danger btn-sm me-1">apagar</button>
+                    <button @click="edit(product._id)" class="btn btn-primary btn-sm">atualizar</button>
                   </td>
                 </tr>
               </tbody>
@@ -57,12 +92,14 @@
     data() {
       return {
         create: true,
+
         product: {
+          _id: '',
           name: '',
           desc: '',
-          price: 0,
-          _id: ''
+          price: 0
         },
+
         products: []
       }
     },
@@ -71,9 +108,19 @@
 
       submit()
       {
-        if (this.create === true) {
-          axios.post('http://localhost:3030/product')
-          .then(response => console.log(response))
+        if (this.create === true)
+        {
+          let product = {
+            name: this.product.name,
+            desc: this.product.desc,
+            price: this.product.price
+          }
+
+          axios.post('http://localhost:3030/product', product)
+          .then(response => {
+            console.log(response)
+            this.resetForm()
+          })
           .catch(err => console.log(err))
         }
         else {
@@ -115,6 +162,17 @@
 
         this.product = productFroEdit [0]
         console.log(this.product)
+      },
+
+      resetForm(create = true)
+      {
+
+        this.product._id = ''
+        this.product.name = ''
+        this.product.desc = ''
+        this.product.pricee = ''
+
+        this.create = create
       }
     }
   }
@@ -122,11 +180,11 @@
 
 <style>
 
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
 *
 {
-  font-family: 'Noto Sans JP', sans-serif !important;
+  font-family: 'Raleway', sans-serif;
 }
 
 body
@@ -134,11 +192,10 @@ body
   background-color: #eaeaea !important;
 }
 
-form.product-data button
+.title-screen
 {
-  text-transform: uppercase;
-  font-weight: 600;
-  font-size: 15px
+  /* max-width: 1143px; */
+  background-color: #fff;
 }
 
 </style>
